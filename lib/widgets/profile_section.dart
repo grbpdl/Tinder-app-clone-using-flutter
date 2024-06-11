c31e67d74325ba714e37cdbe0305a3a3553ab151
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-06-11 11:04:17 +0545

    like dislike with slide feature added

--- lib/widgets/profile_section.dart
+++ lib/widgets/profile_section.dart
@@ -1,6 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:swipable_stack/swipable_stack.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -31,9 +42,9 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
+      width: 350,
       height: 700,
-      color: Colors.amber,
+      color: const Color.fromARGB(255, 255, 255, 255),
       child: SwipableStack(
         swipeAssistDuration: const Duration(milliseconds: 500),
         detectableSwipeDirections: const {
@@ -50,22 +61,102 @@ class _ProfileState extends State<Profile> {
         horizontalSwipeThreshold: 0.8,
         verticalSwipeThreshold: 0.8,
         builder: (context, properties) {
-          final itemIndex = properties.index % 4; //_images.length
+          final itemIndex = properties.index % imgList.length; //
 
           return Stack(
+            alignment: AlignmentDirectional.center,
             children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 500,
+              Positioned.fill(
+                child: Image.asset(
+                  'assets/photos/individual2.jpg',
+                  height: double.maxFinite,
+                  width: double.maxFinite,
+                ),
               ),
               // more custom overlay possible than with overlayBuilder
-              if (properties.stackIndex == 0 && properties.direction != null)
-                Text('$properties.swipeProgress' '$properties.direction'),
-              // CardOverlay(
-              //   swipeProgress: properties.swipeProgress,
-              //   direction: properties.direction!,
-              // )
+              if (properties.stackIndex == 0 &&
+                  properties.direction == SwipeDirection.right)
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.green,
+                      width: 5,
+                    ),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ),
+                  child: const Text(
+                    'Liked',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              if (properties.stackIndex == 0 &&
+                  properties.direction == SwipeDirection.left)
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 5,
+                    ),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ),
+                  child: const Text(
+                    'Dislike',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              Positioned(
+                bottom: 90,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 80.0,
+                        height: 80.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            width: 2.0,
+                          ),
+                        ),
+                        child: const Icon(
+                          Icons.thumb_down,
+                          size: 60,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              width: 2.0,
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.thumb_up,
+                            size: 60,
+                            color: Colors.green,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
             ],
           );
         },
